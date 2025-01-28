1feb8ce9def51b9b019c87483936514eb4b8d2e6
Mohammad Bahmani <bahmani.m.n@gmail.com>
2025-01-28 09:33:10 +0100

    refine the app and GUI

--- app_wine_clf/app.py
+++ app_wine_clf/app.py
@@ -12,17 +12,17 @@ MLFLOW_MODEL_ENDPOINT = f"{ENDPOINT_URL}/invocations"
 
 # Categories list
 CATEGORIES = [
-    "fixed acidity",
-    "volatile acidity",
-    "citric acid",
-    "residual sugar",
-    "chlorides",
-    "free sulfur dioxide",
-    "total sulfur dioxide",
-    "density",
-    "pH",
-    "sulphates",
-    "alcohol"
+    "Fixed Acidity",
+    "Volatile Acidity",
+    "Citric Acid",
+    "Residual Sugar",
+    "Chlorides",
+    "Free Sulfur Dioxide",
+    "Total Sulfur Dioxide",
+    "Density",
+    "PH",
+    "Sulphates",
+    "Alcohol"
 ]
 
 @app.route("/", methods=["GET", "POST"])
@@ -49,7 +49,7 @@ def index():
             # Check if the prediction matches the actual class
             if actual_class:
                 if int(prediction) == int(actual_class):
-                    message = "The model was correct!"
+                    message = "The model correctly predicted the wine quality!"
                 else:
                     message = "The model did not deliver the correct class! Maybe next time!"
         except Exception as e:
@@ -57,48 +57,5 @@ def index():
 
     return render_template("index.html", categories=CATEGORIES, prediction=prediction, message=message)
 
-# # Frontend template
-# TEMPLATE = """
-# <!doctype html>
-# <html lang="en">
-# <head>
-#     <meta charset="UTF-8">
-#     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-#     <title>Wine Quality Prediction</title>
-# </head>
-# <body>
-#     <h1>Wine Quality Prediction</h1>
-#     <form method="POST">
-#         <h3>Input Features</h3>
-#         {% for category in categories %}
-#             <label for="{{ category }}">{{ category }}</label>
-#             <input type="number" step="any" name="{{ category }}" required><br>
-#         {% endfor %}
-
-#         <h3>Actual Class (Optional)</h3>
-#         <label for="actual_class">Actual Wine Quality Class</label>
-#         <input type="number" name="actual_class"><br>
-
-#         <button type="submit">Submit</button>
-#     </form>
-
-#     {% if prediction is not none %}
-#         <h3>Prediction</h3>
-#         <p>Predicted Wine Quality Class: {{ prediction }}</p>
-#     {% endif %}
-
-#     {% if message %}
-#         <h3>Message</h3>
-#         <p>{{ message }}</p>
-#     {% endif %}
-# </body>
-# </html>
-# """
-
-# @app.route("/template")
-# def template():
-#     return TEMPLATE
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-    # app.run(host="0.0.0.0", port=5000, debug=True)
